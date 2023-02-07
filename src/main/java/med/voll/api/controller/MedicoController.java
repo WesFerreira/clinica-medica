0e6083ef9a5ff6a3d0d3eb51c28088bb0c682469
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-02-06 21:19:08 -0300

    Padronizando retornos seguindo o protocolo HTTP e criação de Records de detalhamento para medico e paciente

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,17 +1,16 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosAtualizacaoMedico;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedicos;
-import med.voll.api.medico.Medico;
+import med.voll.api.medico.*;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,37 +21,48 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-        repository.save(new Medico(dados));
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        repository.save(medico);
+
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
+
     }
 
     @GetMapping
     //@PageableDefault() para alterar os padrões. (size, page, sort)
-    public Page<DadosListagemMedicos> listar(@PageableDefault (size = 10, sort = {"nome"}) Pageable paginacao) {
+    public ResponseEntity<Page<DadosListagemMedicos>> listar(@PageableDefault (size = 10, sort = {"nome"}) Pageable paginacao) {
         //conversão de Medicos para DadosListagemMedicos, usando .stream() e fazer um mapeamento com .map()
         //DadosListagemMedicos::new chama o construtor de DadosListagemMedicos
         //.toList() para converter em uma lista
 
         //para retornar todos os dados do banco, mesmo estando inativo
         //return repository.findAll(paginacao).map(DadosListagemMedicos::new);
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedicos::new);
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedicos::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     //  entre {} parametros dinamicos
     @DeleteMapping("/{id}")
     @Transactional
     //@PathVariable para informar que o id do parametro é o mesmo id da url
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         // método para excluir direto no banco de dados.
         //repository.deleteById(id);
         var medico = repository.getReferenceById(id);
         medico.deixarInativo();
+        //noContent() para retornar o 204 e .buil() porq o noContent() ele não devolve o ResponseEntity e o .build() sim
+        return ResponseEntity.noContent().build();
     }
 }
