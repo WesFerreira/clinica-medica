4ad9a5767d7e1639d0832d4a74e61aee94d4069e
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-02-07 23:38:48 -0300

    criação da pasta e classe de usuario

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String login;
+    private String senha;
+}
