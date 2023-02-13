4983cedf51065ea1cd4a09471473a92d5c986ac8
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-02-13 13:28:17 -0300

    criação da classe TokenService

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,35 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario) {
+        try {
+            var algoritmo = Algorithm.HMAC256("123456");
+            return JWT.create()
+                    .withIssuer("API voll.med")
+                    //quem é a pessoa relacionada com esse token
+                    .withSubject(usuario.getLogin())
+                    // configurando quanto tempo estara disponivel esse token
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("erro ao gerar token JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
