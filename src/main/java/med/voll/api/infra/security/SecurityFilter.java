e2f74df13e07285026db1cfcc96af1f51fc77d4f
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-02-14 12:02:33 -0300

    Criação do Filter, SecurityFilter

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,22 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+//Anotação para carregar uma classe que não tem um tipo(Service, Repository), é um tipo genérico
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+
+    }
+}
